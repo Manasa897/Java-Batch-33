3229565ebfa3bd58da65ff9f9563cb155c0e1f7b
Manasa <mallelamanasa97@gmail.com>
2024-01-11 14:07:07 -0600

    inital

---
+++ assign/src/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    String name="Manasa";
+    public String getName() {
+
+        return name;
+    }
+    public static void main(String[] args)
+    {
+        Student stu=new Student();
+        System.out.println(" "+stu.getName());
+    }
+}
