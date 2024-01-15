d0d00d2e0821c377768f75afa116dbbf7036f5ce
Manasa <mallelamanasa97@gmail.com>
2024-01-14 21:34:13 -0600

    Modified

---
+++ assign/src/oops/Animal.java
@@ -0,0 +1,23 @@
+public class Animal {
+    public void makeSound() {
+        System.out.println("Some generic animal sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Bark");
+    }
+
+
+}
+
+class PolymorphismExample {
+    public static void main(String[] args) {
+        Animal myAnimal = new Dog();
+        myAnimal.makeSound();
+
+        System.out.println(myAnimal); // Outputs "This is a Dog object"
+    }
+}
