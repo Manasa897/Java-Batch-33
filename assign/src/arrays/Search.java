4efeffbbef7558be9e9968e2aac97e898cc4377e
Manasa <mallelamanasa97@gmail.com>
2024-01-18 11:53:38 -0600

    new

---
+++ assign/src/arrays/Search.java
@@ -0,0 +1,32 @@
+package arrays;
+
+public class Search {
+    public static void main(String[] args) {
+        int[] sortedArray = {5, 10, 15, 20, 25};
+        int target = 20;
+        int result = binarySearch(sortedArray, target);
+        if (result != -1) {
+            System.out.println("Element " + target + " found at index " + result);
+        } else {
+            System.out.println("Element " + target + " not found in the array");
+        }
+    }
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
