2999c99261a79a7c581407eb194dcd3a1012e129
Manasa <mallelamanasa97@gmail.com>
2024-01-24 12:04:17 -0600

    Test

---
+++ src/test/java/list/LastObject.java
@@ -0,0 +1,17 @@
+package list;
+
+import java.util.ArrayList;
+public class LastObject {
+    public static void main(String[] args) {
+        ArrayList<String> inp = new ArrayList<>();
+        inp.add("adding 1st element");
+        inp.add("adding 2nd element");
+        inp.add("adding 3rd element");
+        inp.add("adding 4th element");
+        int l=inp.size();
+        inp.remove(l-1);
+        for (String s : inp) {
+            System.out.println(s);
+        }
+    }
+}
