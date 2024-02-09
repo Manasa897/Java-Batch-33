08ff441dc24f4c0ade68160badaa17adcfeb85af
Manasa <mallelamanasa97@gmail.com>
2024-02-09 11:46:23 -0600

    Update

---
+++ src/test/java/lambdas/CapitalizeTest.java
@@ -0,0 +1,12 @@
+package lambdas;
+import lambdas.Capitalize;
+import org.junit.Test;
+
+public class CapitalizeTest {
+    @Test
+    public void captitaltest() {
+        String[] strings = {"apple", "banana", "orange", "kiwi", "grape"};
+        Capitalize.Captial(strings);
+
+    }
+}
---
+++ src/test/java/threads/OddandEvenTest.java
@@ -0,0 +1,14 @@
+package threads;
+
+import org.junit.Test;
+import threads.OddandEven;
+
+public class OddandEvenTest {
+    @Test
+    public void oddandeventest() {
+        OddandEven evenThread = new OddandEven();
+        OddandEven oddThread = new OddandEven();
+        evenThread.start();
+        oddThread.start();
+    }
+}
